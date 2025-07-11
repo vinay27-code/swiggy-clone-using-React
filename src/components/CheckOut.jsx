307268467ffd38d66dcd1928b8e202881ef30a52
Vinay Babu Machha <vinaybabumachha27@gmail.com>
2025-07-11 02:01:13 -0700

    checkout program updated

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react"; 
 import { useSelector, useDispatch } from "react-redux";
 import { decreaseQuantity, increaseQuantity, clearCart } from "../cartStore/cartSlice";
 
